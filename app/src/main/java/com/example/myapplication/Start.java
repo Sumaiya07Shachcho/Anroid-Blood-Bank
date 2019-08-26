9ce284a58581d0cbd9e59c53d1f03e294ce06301
Abulkawsar <45530208+Abulkawsar@users.noreply.github.com>
2019-08-27 00:11:50 +0600

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication/Start.java
@@ -0,0 +1,37 @@
+package com.kayeswap.bloodbank;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.kayeswap.shahajjo.R;
+
+public class Start extends AppCompatActivity {
+
+    ImageView Start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Start = findViewById(R.id.start);
+
+        Animation myFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        Start.startAnimation(myFadeInAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mIntent = new Intent(Start.this, Welcome.class);
+                startActivity(mIntent);
+                finish();
+            }
+        }, 3000);
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/Welcome.java
@@ -0,0 +1,31 @@
+package com.kayeswap.bloodbank;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.kayeswap.shahajjo.R;
+
+public class Welcome extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_welcome);
+
+            button = findViewById(R.id.open);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(Welcome.this, Blood_Bank.class));
+                }
+
+
+            });
+        }
+}
